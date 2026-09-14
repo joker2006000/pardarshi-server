1ae5a6e2f53b0f37b02582dda9fbf6373a702bfd
DADY <dady@INBOOK_Y1_PLUSgit config --global --listgit config --global user.email dady@INBOOK_Y1_PLUS>
2026-09-15 05:13:05 +0530

    Add clickable QR link for Render logs

--- puppeteer.config.cjs
+++
@@ -1,6 +0,0 @@
-const { join } = require('path');
-
-module.exports = {
-  // This forces Puppeteer to download Chrome inside your project folder so Render cannot delete it
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
